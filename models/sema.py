fe9887ee42a751ec6f2792124bbcee6a89d70e40
Saptarshi Samanta <36296989+saptarshi2000@users.noreply.github.com>
2026-04-06 18:31:17 +0530

    Enhance logging of class means in sema.py
    Added rounding of class means for better logging clarity.

--- models/sema.py
+++ models/sema.py
@@ -278,7 +278,8 @@ class Learner(BaseLearner):
         logging.info("Task {}: class means updated for {}".format(self._cur_task, seen_classes))
         for c in seen_classes:
             logging.info("  Class {:3d}: norm={:.4f}, total_samples={}".format(
-                c, np.linalg.norm(self._sema_class_means[c]), self._class_sample_counts[c]
+                c, np.linalg.norm(self._sema_class_means[c]), self._class_sample_counts[c],
+                np.round(self._sema_class_means[c][:5], 4)
             ))
 
         self._network.train()
